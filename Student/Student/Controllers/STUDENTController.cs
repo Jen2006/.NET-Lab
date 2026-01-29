a1370351297f3d870549f52a0ae0d25c6219d084
Jenisha Castelino <jenishacastelino8@gmail.com>
2026-01-29 12:21:53 +0530

    file added

---
+++ Student/Student/Controllers/STUDENTController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Student.Models;
+
+
+namespace Student.Controllers
+{
+    public class STUDENTController : Controller
+    {
+        public IActionResult Index()
+        {   
+            List <STUDENT> students = new List<STUDENT>
+            {
+                new STUDENT { USN = 09, Name = "Onovesho", Sub1 = 85, Sub2 = 90, Sub3 = 78 },
+                new STUDENT { USN = 32, Name = "Jenisho", Sub1 = 88, Sub2 = 76, Sub3 = 92 },
+                new STUDENT { USN = 58, Name = "Rohul", Sub1 = 90, Sub2 = 89, Sub3 = 84 }
+            };
+
+
+            return View(students);
+        }
+    }
+}
---
+++ Student/Student/Models/STUDENT.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Student.Models
+{
+    public class STUDENT
+    {
+        [Required]
+        public int USN { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int Sub1 { get; set; }
+
+        public int Sub2 { get; set; }
+
+        public int Sub3 { get; set; }
+
+
+
+
+
+
+    }
+}
