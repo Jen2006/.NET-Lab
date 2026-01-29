c41c1ce7f54b49d1ca5f6ce10f2df7c1ecf2b35b
Jenisha Castelino <jenishacastelino8@gmail.com>
2026-01-29 09:44:40 +0530

    Added Employee project

---
+++ Employee/Employee/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Employee.Models;
+
+namespace Employee.Controllers 
+{   
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<emp> employees = new List<emp>()
+            {
+                new emp { Id = 1, Name = "Ravi", Department = "IT" },
+                new emp { Id = 2, Name = "Anu", Department = "HR" }
+            };
+
+            return View(employees);
+        }
+    }
+}
